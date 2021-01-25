567b388a69966dba84182bf9dad08833aad6085f
Maria4lexzy <37138736+Maria4lexzy@users.noreply.github.com>
2021-01-25 15:29:35 +0100

    firebase signup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Signup from "./components/Signup";
 import { Container } from 'react-bootstrap';
+import { AuthProvider } from './contexts/AuthContext';
 function App() {
   return (
-    <Container className="d-flex alighn-items-center justify-content-center" style={{ minHeight: "100vh" }}>
-      <div className="w-100" style={{ maxWidth: "400px" }}> <Signup /></div>
-<h2>I made a change again
-  
-</h2>
-    </Container>
+    <AuthProvider>
+      <Container className="d-flex alighn-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+        <div className="w-100" style={{ maxWidth: "400px" }}> <Signup /></div>
+        <h2>I made a change again</h2>
+      </Container>
+    </AuthProvider>
   );
 }
 
