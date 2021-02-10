5098c19d09f926062fdf3e840bd050c429735f73
Maria4lexzy <37138736+Maria4lexzy@users.noreply.github.com>
2021-02-10 01:22:30 +0100

    Authentication
    Firebase Authentication + Creation of Home, Account, Signup, SignIn, SignOut, PasswordReset, PasswordForget.
    Navbar for auth user and none auth user
    Admin page
    Refactor React Routing

--- src/components/oldSgnup.js
+++ src/components/oldSgnup.js
@@ -6,94 +6,76 @@
 // import * as ROUTES from '../constants/routes';
 
 // export default function Signup() {
-
-
 //     const emailRef = useRef();
 //     const passwordRef = useRef();
 //     const passwordConfirmRef = useRef();
-//     const { currentUser, updateEmail, updatePassword } = useAuth();
+//     const { signup } = useAuth();
 //     const [error, setError] = useState('');
 //     const [loading, setLoading] = useState(false);
-//     const [message, setMessage] = useState('');
 //     const history = useHistory;
 
-//     function handleSubmit(e) {
+
+//     async function handleSubmit(e) {
 //         e.preventDefault();
 //         if (passwordRef.current.value !== passwordConfirmRef.current.value) {
 //             return setError('Passwords do not match');
-//         }
-//         const promises = [];
-//         setLoading(true);
-//         setError('');
-//         setMessage('');
-//         if (emailRef.current.value !== currentUser.email) {
-//             promises.push(updateEmail(emailRef.current.value));
-//         }
-//         if (passwordRef.current.value) {
-//             promises.push(updatePassword(passwordRef.current.value));
-//         }
 
-//         Promise.all(promises)
-//             .then(() => {
-//                 history.push('/');
-//                 setError('Profile Information Updated')
-//             }).catch(() => {
-//                 setError('Failed to update account')
-//             }).finally(() => {
-//                 setLoading(false);
-//                 setMessage('Account Successfully Updated');
-
-//             })
-
+//         }
+//         try {
+//             setError('');
+//             setLoading(true);
+//             await signup(emailRef.current.value, passwordRef.current.value);
+//             history.push("/");
+//         } catch {
+//             setError('Failed to create an account');
+//         }
+//         setLoading(false);
 
 //     }
-
-
 //     return (
 //         <>
 //             <Container className="d-flex alighn-items-center justify-content-center" style={{ minHeight: "100vh" }}>
 //                 <div className="w-100" style={{ maxWidth: "400px" }}>
 //                     <Card>
 //                         <Card.Body>
-//                             <h2 className="text-center">Update Profile</h2>
+//                             <h2 className="text-center">Sign Up</h2>
 //                             {error && <Alert variant="danger">{error}</Alert>}
-//                             {message && <Alert variant="success">{message}</Alert>}
 //                             <Form onSubmit={handleSubmit}>
 //                                 <Form.Group id="email">
 //                                     <Form.Label>Email</Form.Label>
-//                                     <Form.Control type="email" ref={emailRef} required defaultValue={currentUser.email}></Form.Control>
+//                                     <Form.Control type="email" ref={emailRef} required></Form.Control>
 //                                 </Form.Group>
 
 //                                 <Form.Group id="password">
 //                                     <Form.Label>Password</Form.Label>
-//                                     <Form.Control type="password" ref={passwordRef} placeholder="Leave blank to keep the same"></Form.Control>
+//                                     <Form.Control type="password" ref={passwordRef} required></Form.Control>
 //                                 </Form.Group>
 
 //                                 <Form.Group id="password-confirm">
 //                                     <Form.Label>Password Confirmation</Form.Label>
-//                                     <Form.Control type="password" ref={passwordConfirmRef} placeholder="Leave blank to keep the same"></Form.Control>
+//                                     <Form.Control type="password" ref={passwordConfirmRef} required></Form.Control>
 //                                 </Form.Group>
-//                                 <Button disabled={loading} className="w-100" type="submit" >Update Profile</Button>
+//                                 <Button disabled={loading} className="w-100" type="submit" >Sign Up</Button>
 //                             </Form>
 //                         </Card.Body>
 //                     </Card>
 //                     <div className="w-100 text-center mt-2">
-//                         <Link to={ROUTES.DASHBOARD} >Cancel</Link>
+//                         Already have an account? <Link to={ROUTES.LOG_IN} >Log In</Link>
 //                     </div>
+
 //                 </div>
 //             </Container>
 //         </>
 //     );
 // }
+
+
 import React from 'react'
 
-export default function UpdateProfile() {
+export default function oldSign() {
     return (
         <div>
 
         </div>
     )
 }
-
-
-
--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,10 +1,9 @@
-export const DASHBOARD = '/';
-export const SIGN_UP = '/signup';
-export const LOG_IN = '/login';
 export const CALENDAR = '/calendar';
 export const CALENDAR_T = '/calendar-test';
-// // export const HOME = '/home';
-// export const ACCOUNT = '/account';
-// export const ADMIN = '/admin';
-// export const CALENDAR = '/calendar';
-export const FORGET_PASSWORD = '/forgot-password';
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const HOME = '/home';
+export const ACCOUNT = '/account';
+export const ADMIN = '/admin';
+export const PASSWORD_FORGET = '/pw-forget';
+export const LANDING = '/';
